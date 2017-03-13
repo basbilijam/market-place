20b41f9a6b5c58a7502ceeb9505841d2fcd14c48
basbilijam <basbilijam@gmail.com>
2017-03-13 16:24:46 +0100

    registration route, module and view added

---
+++ routes/register.js
@@ -0,0 +1,34 @@
+const express = require ('express')
+const router = Express.router()
+const sequelize = require ('sequelize')
+const bodyParser = require('body-parser')
+
+// connecting to databse
+const db = require('../modules/m-db')
+
+// setting router sub app to use bodyparser
+router.use(bodyParser.urlencoded({ extended: false }))
+
+// setting up route ro register page
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// posting new user to database
+router.post('/register', (req, res) => {
+  const newUser = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    listing: req.body.listing,
+    companyname: req.body.companyname,
+    location: req.body.location,
+    type: req.body.type  
+  }
+  console.log(req.body)
+  db.User.create(newUser).then()
+  console.log(newUser)
+  res.redirect('/')
+})
+
+module.exports = router
