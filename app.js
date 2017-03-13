3d6f416a277a8aef2f164f832f48f29fcc354bff
wizardoftomorrow <nyle@Nyles-MacBook-Pro.local>
2017-03-13 16:22:30 +0100

    added login route view and modulde added

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 
 const express = require ('express'),
-      app = express();
+      session = require ('express-session'),
+      app = express(),
+      authentication = require(__dirname +'/routes/authentication');
+      //register = require(__dirname +'/routes/register');
 
-
+// sessions
+app.use(session({
+      secret: 'secure',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false }
+    }));
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/', express.static( __dirname+ '/public') );
 
-
+app.use('/', authentication);
+//app.use('/', register);
 // route to index page
 app.get('/', (req, res) => {
   res.render('index')
 })
+
 
 
 app.listen(4001, (req, res) => {
