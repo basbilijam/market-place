0128f39ad5ad3275b7547340338297a98d8daa4d
basbilijam <basbilijam@gmail.com>
2017-03-15 17:29:20 +0100

    started on search by location

--- modules/m-search.js
+++ modules/m-search.js
@@ -3,8 +3,9 @@ const express = require('express')
       bodyParser = require ('body-parser')
       db = require (__dirname +'/m-db')
       router = express.Router()
+      googleMapsClient = require('@google/maps').createClient({key: 'AIzaSyBqYbVHxiv8ebhz4tUZVH6Byj7s2A_4HqE'});
 
-// search functionality on username
+//search functionality on username
 const search = router.post('/search', (req, res) => {
   console.log("req body is " +req.body.search)
   let searchQuery = req.body.search
@@ -21,5 +22,20 @@ const search = router.post('/search', (req, res) => {
     console.log(err);
   })
 })
+
+// search googlemaps api
+// const search = router.post('/search', (req, res) => {
+//   googleMapsClient.geocode({
+//     console.log('req body is ', req.body.search)
+//     location: req.body.search
+//   }, function(err, response) {
+//     if (!err) {
+//       console.log('Googlemaps results are: ', response.json.results);
+//     }
+//   });
+// })
+
+
+
 
 module.exports = search
